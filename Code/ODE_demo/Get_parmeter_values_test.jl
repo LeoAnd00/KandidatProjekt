1fe1441879af6c492c5c2fd1d41253a520e9b3a0
jespeols <99250520+jespeols@users.noreply.github.com>
2022-02-16 21:59:48 +0100

    Börjat implmenetera ODE-ekv + parametrar

--- Code/ODE_demo/Get_parmeter_values_test.jl
+++
@@ -1,2 +0,0 @@
-# Parmaterar för ODE, samma sätt 1=x, 2=y, 3=z
-dict1 = Dict("P1" => 10.0, "P2" => 28.0, "P3"=>8/3)
--- Code/ODE_demo/ODE_functions_test.jl
+++ Code/ODE_demo/ODE_functions_test.jl
@@ -1,9 +1,19 @@
-#Definerar ODE- systemes 3 ekvationer
-# u[1]=x, u[2]=y, u[3]=z
-include("Get_parmeter_values_test.jl")
+# 1=Glutamin, Cyr1=2, 3=Ras, 4=EGO, 5= EGOGAP, 6= cAMP
+#  7=PDE, 8=Sak 9=TORC1, 10=Snf1, 11=PKA, 12=Sch9
+# 13=Gcn2, 14=Gcn4, 15=eIF, 16=Gln3, 17=Gln1, 18=Rtg13
+# 19=Gis1, 20=Mig1, 21=Dot6 22=Tps1, 23=Trehalase
+# 24=Protein, 25=Rib
 
-function ODE_test(du, u,p, t)
-    du[1] = dict1["P1"]*(u[2]-u[1])
-    du[2] = u[1]*(dict1["P2"]-u[3]) - u[2]
-    du[3] = u[1]*u[2] - dict1["P3"]*u[3]
+include("Get_parameter_values_test.jl")
+
+function soft_Heaviside(sigma, W)
+    1/(1+exp(-sigma*W))
 end
+
+function ODE_functions(du, u, t)
+    du[22] = Tps1_parameters["gammatps"](*u[22]*soft_Heaviside(Tps1_parameters["sigma_tps"], Tps1_parameters["w_tps_pka"]*(u[?] - u[11])))
+    du[23] = Trehalase_parameters["gammatre"]*(u[?]*soft_Heaviside(Trehalase_parameters["sigma_trehalase"], Trehalase_parameters["w_tre_pka"]*u[11]-Trehalase_parameters["w_tre"])-u[23])
+    du[24] = Protein_parameters["k_pr"]*Protein_parameters["ATP"]*minimum([minimum([u[25] u[15]]) minimum(Protein_parameters["tRNA_total"] u[1])])*u[24]
+    du[25] = Rib_parameters["k_transcription"]*(1-u[21])-Rib_parameters["k_mRNA_degr"]*u[25]
+end
+
