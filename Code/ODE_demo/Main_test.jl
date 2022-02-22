442081b8ecc1648ad772a3f1bb707a37ffef8a08
jespeols <99250520+jespeols@users.noreply.github.com>
2022-02-22 13:51:12 +0100

    Färdig implementering samt lösning av Jalihal's ODE-modell
    Använder standardlösare

--- Code/ODE_demo/Main_test.jl
+++
@@ -1,7 +0,0 @@
-# Löser ODEer i ODE_functions, samma index som där med [1]=x, [2]=y, [3]=z
-using DifferentialEquations
-include("ODE_functions_test.jl"), ("Get_parmeter_values_test.jl")
-u0=[1.0, 0.0, 0.0]
-tspan=(0.0, 100.0)
-prob=ODEProblem(ODE_test, u0, tspan)
-sol=solve(prob)
--- Code/ODE_demo/test.jl
+++
@@ -1,2 +0,0 @@
-Cyr1_parameters = Dict("gammacyr" => 8.96, "Cyr1_T" => 1.0, "w_cyr_glu" => 5.13, "w_cyr_snf" => 0.12, "sigma_cyr" => 3.5, "w_cyr" => 1.35)
-
