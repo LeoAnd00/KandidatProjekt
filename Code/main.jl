b46da7c950fd203aabb1d6301a199812586f704a
jespeols <99250520+jespeols@users.noreply.github.com>
2022-04-13 20:00:07 +0200

    Fixat konsekvenser från omorganisation av koden

--- Code/main.jl
+++ Code/main.jl
@@ -1,6 +1,6 @@
 using LatinHypercubeSampling
 include("Parameter_Estimation/param_estim.jl")
-include("Solve_Jalihal_ODE/Time_course/ODE_methods.jl")
+include("Model/ODE_methods.jl")
 
 #p0 via latin hypercube på logskala (log 10) från -3 till 3
 n_ps = 2 #ska vara antal p_var
