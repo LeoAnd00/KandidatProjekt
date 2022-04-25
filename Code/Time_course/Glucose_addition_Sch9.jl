34956f31a7cb4b8b2b55c46f967eae401f054f58
Emmajoh00 <99248440+Emmajoh00@users.noreply.github.com>
2022-04-25 20:35:10 +0200

    Commented code

--- Code/Time_course/Glucose_addition_Sch9.jl
+++ Code/Time_course/Glucose_addition_Sch9.jl
@@ -1,4 +1,4 @@
-# 
+# Creates the plot cAMP under glucose addition with pre- and postshift variables
 
 using DifferentialEquations
 using ModelingToolkit
@@ -7,21 +7,22 @@ using Plots
 include("../Model/ODE_functions.jl") 
 
 include("../Model/parameter_values.jl")
-# Mutant Sch9_Delta => Sch9_T = 0
+# Mutant Sch9_Delta => Sch9_T = 0, Creates a mutant by deleting Sch9_T
 p_const[Get_index(p_const_lookup_table, "Sch9_T")] = Sch9_T => 0.0
 
-# Pre-shift => ATP, Carbon = 0
+# Pre-shift => ATP, Carbon = 0, which is glucos starvation
 include("../Model/ODE_methods.jl")
-u0_SS = Steady_state_solver(p_const, p_var, (Carbon => 0.0, ATP => 0.0, Glutamine_ext => 1.0)) # Returnerar steady state för parametrarna p
+u0_SS = Steady_state_solver(p_const, p_var, (Carbon => 0.0, ATP => 0.0, Glutamine_ext => 1.0)) # Return steady state parameters for glucos starvation
 
-# Post-shift => ATP, Carbon = 1
+# Post-shift => ATP, Carbon = 1, which is glucos addition 
 tspan = (0.0, 3.0) # [min]
-sol = ODE_solver(u0_SS, (Carbon => 1.0, ATP => 1.0, Glutamine_ext => 1.0), tspan, p_const, p_var)
+sol = ODE_solver(u0_SS, (Carbon => 1.0, ATP => 1.0, Glutamine_ext => 1.0), tspan, p_const, p_var)  # Solves the ODEs for the u0 from glucos addition over tspan
 
+# Makes a scatter-plot from the experimental data from cAMP 
 include("../../Data/exp_data_norm.jl")
-
 scatter(t_sch9Delta_cAMP, data_sch9Delta_cAMP)
 
+# Plot results from simulation and experimental data
 plot1 = plot!(sol, vars=cAMP, legend=false)
 xlabel!("t [min]")
 ylabel!("cAMP")
--- Code/Time_course/Rapamycin_treatment.jl
+++ Code/Time_course/Rapamycin_treatment.jl
@@ -1,4 +1,4 @@
-# 
+# Creates the plot for Rapamycin treatment where Ribi is used to make the plot
 
 using DifferentialEquations
 using ModelingToolkit
@@ -10,22 +10,23 @@ include("../Model/parameter_values.jl")
 # Pre-shift => No change in parameter values
 
 include("../Model/ODE_methods.jl")
-u0_SS = Steady_state_solver(p_const, p_var, (Carbon => 1.0, ATP => 1.0, Glutamine_ext => 1.0)) # Returnerar steady state för parametrarna p
+u0_SS = Steady_state_solver(p_const, p_var, (Carbon => 1.0, ATP => 1.0, Glutamine_ext => 1.0)) # Returns steady state for the parameters p
 
-# Post-shift: Rapamycin treatment => TORC1_T = 0.0 
+# Post-shift: Rapamycin treatment which means TORC1_T = 0.0 
 p_const[Get_index(p_const_lookup_table, "TORC1_T")] = TORC1_T => 0.0
 
-# Steady-state vid svältning utgör initialvärden för Glutamine addition
+# Steady-state at starvation which is the initial values for Glutamine addition
 tspan = (0.0, 90.0) # [min]
-sol = ODE_solver(u0_SS, (Carbon => 1.0, ATP => 1.0, Glutamine_ext => 1.0), tspan, p_const, p_var)
+sol = ODE_solver(u0_SS, (Carbon => 1.0, ATP => 1.0, Glutamine_ext => 1.0), tspan, p_const, p_var) # Solve all ODEs
 
-# Rel. RPL32 mRNA motsvarar (Rib/steady-state värde för Rib)
+# Rel. RPL32 mRNA corresponds to (Rib/steady-state value for Rib)
 include("../../Data/exp_data_norm.jl")
 
+# Plot the results from the simulations and data from a seperate file
 plot1 = scatter(t_Rib_rap, data_Rib_rap)
 
 plot!(sol, vars=Rib/(1e-3+u0_SS[25]), legend=false) # 1e-2 ensures denom != 0
 xlabel!("t [min]")
-ylabel!("Rel. RPL32 mRNA") # Motsvarar konc. Rib relativt steady state
+ylabel!("Rel. RPL32 mRNA") # Corresponds to Rib relativt steady state
 title!("Rapamycin treatment, TORC1_T = 0")
 display(plot1)
